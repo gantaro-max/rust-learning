dafa6c600f047ad0e514640546707939a3844066
gantaro <gantaro1967@gmail.com>
2026-03-17 16:09:03 +0000

    モックテスト作成

--- src/services.rs
+++ src/services.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use crate::error::AppError;
 use crate::models::{DeleteRequest, Item, UpdateStockRequest};
-use crate::repositories::item_repository::{ItemRepository, ItemRepositoryTrait};
+use crate::repositories::item_repository::ItemRepositoryTrait;
 pub struct ItemService{
     repository:Arc<dyn ItemRepositoryTrait>,
 }
@@ -50,34 +50,53 @@ impl ItemService {
 mod tests{
     use super::*;
     use crate::models::{Category, Item};
-    use sqlx::PgPool;
     use async_trait::async_trait;
 
-    struct MockRepository;
+    struct MockRepository{
+        items: Vec<Item>,
+    }
 
     #[async_trait]
     impl ItemRepositoryTrait for MockRepository {
         async fn create(&self, item: Item) -> Result<Item, AppError> {
-            Ok(item) // 投げたものをそのまま返すだけ（DB操作なし！）
+            Ok(item) 
         }
         async fn fetch_all(&self) -> Result<Vec<Item>, AppError> {
-            Ok(vec![]) // 空のリストを返す
+            Ok(self.items.clone()) 
         }
-        async fn update_stock(&self,up_req:&UpdateStockRequest)->Result<u64,AppError>{
+        async fn update_stock(&self,_up_req:&UpdateStockRequest)->Result<u64,AppError>{
             Ok(1)
         }
-        async fn delete(&self,del_req:&DeleteRequest)->Result<u64,AppError>{
+        async fn delete(&self,_del_req:&DeleteRequest)->Result<u64,AppError>{
             Ok(1)
         }
         
     }
 
     #[tokio::test]
-    async fn test_add_item_empty_name(){
-        let pool = PgPool::connect_lazy("postgres://localhost/dummy").unwrap();
-        let repo = Arc::new(ItemRepository::new(pool));
-        let service = ItemService::new(repo);
+    async fn test_add_item_ok(){        
+        let mock_item = Item {
+            id:None,
+            name: "モック".to_string(),
+            price: 100,
+            stock: 10,
+            category: Category::Fruit,
 
+        };
+        
+        let mock_repo = Arc::new(MockRepository{items:vec![mock_item.clone()]});
+        let service = ItemService::new(mock_repo);
+
+        let result = service.add_items(mock_item).await.unwrap();
+
+        assert_eq!(result.name,"モック");
+        assert_eq!(result.price,100);
+        assert_eq!(result.stock,10);
+        assert_eq!(result.category,Category::Fruit);        
+    }
+
+    #[tokio::test]
+    async fn test_add_item_empty_name(){        
         let invalid_item = Item {
             id:None,
             name: "".to_string(),
@@ -86,6 +105,9 @@ mod tests{
             category: Category::Fruit,
 
         };
+
+        let mock_repo = Arc::new(MockRepository{items:vec![]});
+        let service = ItemService::new(mock_repo);
 
         let result = service.add_items(invalid_item).await;
 
